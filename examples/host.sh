17193784c2060249d4810cb409b11676484f3d23
Thomas Kadauke <tkadauke@imedo.de>
2009-05-17 23:12:55 +0200

    made host example work for different user names

--- examples/host.sh
+++ examples/host.sh
@@ -2,14 +2,16 @@ source $FOOP_PATH/foop.sh
 
 class Host
 
-hostname="enter some hostname here, where you can connect to without a password"
+hostname="" # enter some hostname here, where you can connect to without a password
+username="" # enter a user name here, or leave blank for default user
 
 function Host.initialize {
   @s hostname $1
+  @s username $(oror $2 $USER)
 }
 
 function Host.execute {
-  local result=$(ssh deploy@$(@g hostname) "$1")
+  local result=$(ssh $(@g username)@$(@g hostname) "$1")
   puts "$result"
 }
 
@@ -21,7 +23,7 @@ function Host.ps {
   @t execute "ps aux"
 }
 
-s=$(new Host $hostname)
+s=$(new Host $hostname $username)
 puts "current dir"
 @@ $s ls
 
